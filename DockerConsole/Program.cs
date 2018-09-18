d2a50ee29912147c1a1bd52b60bdb17e06c00ec7
rlopezM3 <raulitosanchez@hotmail.com>
2018-09-18 13:15:45 -0400

    Commit Test.

---
+++ DockerConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DockerConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Ross!!");
+            Console.WriteLine("Hello Ross!!");
+        }
+    }
+}
